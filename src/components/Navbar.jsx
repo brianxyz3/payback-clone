de28922aa213c4144f2cfeae1ec23d0c8564bcdc
BrianXYZ <brianchima22@gmail.com>
2024-11-26 08:31:44 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {ChatBubbleOutlineOutlinedIcon as ChatIcon} from '@mui/icons-material/ChatBubbleOutlineOutlined';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <div>Payback</div>
+            <div><ChatIcon/></div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
