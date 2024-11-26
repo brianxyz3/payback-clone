456924ca95eae662c29184dc5c4f96dab62b5782
BrianXYZ <brianchima22@gmail.com>
2024-11-26 16:46:20 +0100

    added hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+    return (
+        <div className="bg-[#112152] h-80  text-white mt-16">
+            <div className="grid grid-cols-70/30 m-auto">
+                <h1 className="text-6xl font-bold"><span className="text-[#ffd700]">Get your money back</span> from a scam</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
