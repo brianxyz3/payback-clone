f410a1d588ad84456381694ca984a91bc949f4ce
BrianXYZ <brianchima22@gmail.com>
2024-11-27 16:01:12 +0100

    minor changes to code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 
 const Hero = () => {
     return (
-        <div className="bg-[#112152] h-80  text-white mt-16">
-            <div className="grid grid-cols-70/30 m-auto">
-                <h1 className="text-6xl font-bold"><span className="text-[#ffd700]">Get your money back</span> from a scam</h1>
+        <div className=" bg-cover bg-center py-7 text-white">
+            <div className="font-bold lg:w-1/2 w-3/4 flex flex-col justify-center items-center text-center mx-auto">
+                <h4 className="text-xl bg-blue-500 rounded-3xl px-5 py-2">Investment & Trading Scam</h4>
+                <h1 className="text-6xl text-yellow-400">Get Your Money Back <span className="text-blue-600">From a Scam</span></h1>
+                <p className="mb-2">Thousands of clients used our Investigation Report to retrieve their losses. Start with securing a free consultation now</p>
+                <p className="bg-blue-500 rounded-3xl px-5 py-2">Get Your Money Back</p>
             </div>
         </div>
     )
