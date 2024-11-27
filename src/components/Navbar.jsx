ea4788a8f5650fc80cc9e750d2468e1750e1195d
BrianXYZ <brianchima22@gmail.com>
2024-11-27 15:59:39 +0100

    changes to code and added hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
-  return (
-      <nav className="bg-[#112152] text-[#f4f4f4] w-full p-4 fixed top-0 left-0">
-          <div className="flex justify-between">
-              <div className="text-xl font-bold">PAYBACK</div>
+    const [isOpen, setIsOpen] = useState(false);
 
-              <div className="flex gap-4">
-                  <div className="size-5 border-t-2 border-r-2 rounded-tr-md rounded-sm">
-                      <ChatBubbleOutlineOutlinedIcon sx={{ fontSize: 32 }} className="relative right-3.5" />
-                  </div>
-                  <AccountCircleOutlinedIcon sx={{ fontSize: 32 }} className="border-r-2" />
-                  <MenuIcon />
-              </div>
-        </div>
-    </nav>
-  )
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className="md:bg-[#112152] px-3 text-blue-600 md:text-[#f4f4f4] py-4">
+            <div className="flex justify-between md:items-center">
+                <div className="text-2xl font-bold font-sans text-blue-800"><span className="text-blue-300">r</span>ockettarefund.org</div>
+                <div className={`${isOpen ? "block h-60" : "hidden"} md:h-fit md:flex md:justify-evenly md:w-11/12`}>
+                    <div className={"w-3/4 flex"}>
+                        <ul className="flex justify-center border-1 md:border-none pt-3 text-center gap-4 absolute md:static top-20 left-0 right-0 mx-auto flex-col md:flex-row w-1/2 md:w-full">
+                            <li className="border-b-2 border-gray-200 md:border-[#112152] hover:border-b-blue-300 md:hover:-mt-1">HOME</li>
+                            <li className="border-b-2 border-gray-200 md:border-[#112152] hover:border-b-blue-300 md:hover:-mt-1">ABOUT US</li>
+                            <li className="border-b-2 border-gray-200 md:border-[#112152] hover:border-b-blue-300 md:hover:-mt-1">FAQ</li>
+                            <li className="border-b-2 border-gray-200 md:border-[#112152] hover:border-b-blue-300 md:hover:-mt-1">CONTACT US</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="flex items-start gap-4">
+                    <div className="size-5 border-t-2 border-r-2 rounded-tr-md rounded-sm border-blue-600">
+                        <ChatBubbleOutlineOutlinedIcon sx={{ fontSize: 28 }} className="relative right-2.5" />
+                    </div>
+                    <AccountCircleOutlinedIcon sx={{ fontSize: 28 }} />
+                    <button className="block md:hidden text-white bg-blue-500" onClick={toggleNavbar}><MenuRoundedIcon fontSize="large" /></button>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
+
+
+
