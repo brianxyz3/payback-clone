26663a4e5fbab9387032538c21a937f8b2157b7a
BrianXYZ <brianchima22@gmail.com>
2024-11-29 08:29:30 +0100

    features component added

---
+++ src/components/Features.jsx
@@ -0,0 +1,56 @@
+
+const Features = () => {
+
+    return (
+        <section>
+            <div className="my-5 px-6 flex gap-3 justify-evenly flex-wrap lg:justify-between lg:flex-nowrap">
+                <div className="bg-white/50 flex flex-col text-blue-700 hover:text-white hover:bg-blue-700 justify-between w-[29rem] px-4 py-10 rounded-md">
+                    <div className="">
+                        <div className="text-2xl font-bold">
+                            <h5>5 Years</h5>
+                            <h5>of Experience</h5>
+                        </div>
+                        <p className="py-8">
+                            Rocketta Refund has been around for 5 years now. The experience we have accumulated during the years is well worth mentioning. Being highly familiar with most financial authorities worldwide for years gives us an edge and specific relationships that can be used to speed up processes that otherwise would have taken a mucgh longer time, with much higher success rate.
+                        </p>
+                    </div>
+                    <div>
+                        <a href="" className="text-blue-200 bg-blue-600 text-xl font-bold p-2 border border-blue-800 rounded-3xl hover:p-3 hover:border-b-2">Learn More</a>
+                    </div>
+                </div>
+
+                <div className="bg-white/50 flex flex-col text-blue-700 hover:text-white hover:bg-blue-700 justify-between w-[29rem] px-4 py-10 rounded-md">
+                    <div>
+                        <div className="text-2xl font-bold">
+                            <h5>Best</h5>
+                            <h5>Industry Experts</h5>
+                        </div>
+                        <p className="py-8">
+                            Rocketta Refund will never compromise the quality of the staff. Our lawyers, accountants, consultants and service team are all consisted of top-quality experts,with proven real-life-experience and recognition from known Universities.
+                        </p>
+                    </div>
+                    <div>
+                        <a href="" className="text-blue-200 bg-blue-600 text-xl font-bold p-2 border border-blue-800 rounded-3xl hover:p-3 hover:border-b-2">Learn More</a>
+                    </div>
+                </div>
+
+                <div className="bg-white/50 flex flex-col text-blue-700 hover:text-white hover:bg-blue-700 justify-between w-[29rem] px-4 py-10 rounded-md">
+                    <div>
+                        <div className="text-2xl font-bold">
+                            <h5>Fast &</h5>
+                            <h5>Effective Solutions</h5>
+                        </div>
+                        <p className="py-8">
+                            In Rocketta Refund we believe in working Smart AND Hard. We have experts who have both psychology and economy background, which allow them to combine efficiency and smart work and incorporate it into their day-to-day work. This method has been proven to produce faster results compared to methods of other companies, significantly.
+                        </p>
+                    </div>
+                    <div>
+                        <a href="" className="text-blue-200 bg-blue-600 text-xl font-bold p-2 border border-blue-800 rounded-3xl hover:p-3 hover:border-b-2">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Features;
