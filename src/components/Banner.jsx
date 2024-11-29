29cad5d7b95921b52414d6bd6f8b3dbd20362767
BrianXYZ <brianchima22@gmail.com>
2024-11-29 08:51:18 +0100

    banner component added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,36 @@
+import { Check } from "@mui/icons-material";
+
+const Banner = () => {
+    const scamCategories = [
+        "Online Scams",
+        "Crypto Scams",
+        "Marketplace Scams",
+        "Trading Scams",
+        "Work Scams",
+        "Dating Scams",
+        "Investment Scams"
+    ]
+
+
+    return (
+        <section className="bg-blue-700 mt-20">
+            <div className="bg-white slider -rotate-1" data-animated="true">
+                <ul className="slider_inner flex text-blue-700 font-bold text-3xl">
+                    {
+                        scamCategories.map(scam => (
+                            <li><Check fontSize="large" /> {scam}</li>
+                        ))
+                    }
+                    {
+                        scamCategories.map(scam => (
+                            <li aria-hidden="true"><Check fontSize="large" /> {scam}</li>
+                        ))
+                    }
+
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Banner
