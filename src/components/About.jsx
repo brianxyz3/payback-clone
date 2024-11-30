e54c7f694f71cfbe313bd1052f2ab3c263dae59a
BrianXYZ <brianchima22@gmail.com>
2024-11-30 13:33:56 +0100

    added about component

---
+++ src/components/About.jsx
@@ -0,0 +1,47 @@
+const About = () => {
+    return (
+        <section className="">
+            <div id="carouselExampleAutoplaying" className="carousel slide w-2/3 mx-auto my-5 py-1 bg-white rounded-md" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <div className="flex">
+                            <img src="../src/assets/images/logo-1.webp" className="d-block w-1/3" alt="..." />
+                            <img src="../src/assets/images/logo-2.webp" className="d-block w-1/3" alt="..." />
+                            <img src="../src/assets/images/logo-3.webp" className="d-block w-1/3" alt="..." />
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className="flex">
+                            <img src="../src/assets/images/logo-4.webp" className="d-block w-1/3" alt="..." />
+                            <img src="../src/assets/images/logo-5.webp" className="d-block w-1/3" alt="..." />
+                            <img src="../src/assets/images/logo-6.webp" className="d-block w-1/3" alt="..." />
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className="flex">
+                            <img src="../src/assets/images/logo-7.webp" className="d-block w-1/3" alt="..." />
+                            <img src="../src/assets/images/logo-8.webp" className="d-block w-1/3" alt="..." />
+                            <img src="../src/assets/images/logo-9.webp" className="d-block w-1/3" alt="..." />
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className="flex justify-evenly">
+                            <img src="../src/assets/images/logo-10.webp" className="d-block w-1/3" alt="..." />
+                            <img src="../src/assets/images/logo-11.webp" className="d-block w-1/3" alt="..." />
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default About
