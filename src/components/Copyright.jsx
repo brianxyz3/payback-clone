16b5779cbf2697a07559913144a43d6f8e2c0301
BrianXYZ <brianchima22@gmail.com>
2024-12-03 19:49:54 +0100

    added copyright component

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,12 @@
+
+const Copyright = () => {
+    return (
+        <div className="bg-[#112152] py-2 px-5">
+            <div className="border-t text-center text-white">
+                Copyright &copy; 2024.
+            </div>
+        </div>
+    )
+}
+
+export default Copyright;
